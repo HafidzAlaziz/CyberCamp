478d908ab5d3494e51a6df450ac0aa0a2db5c03a
HafidzAlaziz <HafidzAlaziz@users.noreply.github.com>
2026-04-02 20:18:51 +0700

    delete file js script

--- b64.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const flags = ['CTF{XX3_3NT1TY_1NJ3CT10N}', 'CTF{JWT_N0N3_4LG_F0RG3RY}'];
-const out = flags.map(f => Buffer.from(f).toString('base64')).join('\n');
-fs.writeFileSync('b64.txt', out);
-console.log("DONE");
